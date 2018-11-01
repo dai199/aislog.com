29a7139eb710c492275d038ed9a17fc823fcae1e
Tagami Daiki <toda0310@gmail.com>
2018-11-01 12:00:00 +0900

    setlist追加

--- db/fixtures/setlist_details/20161203.rb
+++ db/fixtures/setlist_details/20161203.rb
@@ -1,7 +1,47 @@
 SetlistDetail.seed(:id,
   {
     id: ,
-    setlist_id: ,
+    setlist_id: 154,
+    music_list_id: ,
+    title: "",
+    music_order: ,
+    description: ""
+  },
+  {
+    id: ,
+    setlist_id: 154,
+    music_list_id: ,
+    title: "",
+    music_order: ,
+    description: ""
+  },
+  {
+    id: ,
+    setlist_id: 154,
+    music_list_id: ,
+    title: "",
+    music_order: ,
+    description: ""
+  },
+  {
+    id: ,
+    setlist_id: 154,
+    music_list_id: ,
+    title: "",
+    music_order: ,
+    description: ""
+  },
+  {
+    id: ,
+    setlist_id: 154,
+    music_list_id: ,
+    title: "",
+    music_order: ,
+    description: ""
+  },
+  {
+    id: ,
+    setlist_id: 154,
     music_list_id: ,
     title: "",
     music_order: ,
